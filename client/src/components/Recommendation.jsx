01cc095230033b861ed6f5e3bc56c104a7c3026c
LooKes Wong <lookeswong@gmail.com>
2023-09-13 21:15:47 +0800

    first commit

---
+++ client/src/components/Recommendation.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import Card from './Card';
+
+const Container = styled.div`
+  flex: 2;
+`;
+
+const Recommendation = ({ tags }) => {
+
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+      const fetchVideos = async () => {
+        // const res = await axios.get(`/videos/tags?tags=${tags}`);
+        const res = await axios.get(`/videos/random`);
+        setVideos(res.data);
+      }
+
+      fetchVideos();
+    },[tags])
+
+  return (
+    <Container>
+      {videos.map(video => (
+        <Card type="sm" key={video._id} video={video}/>
+      ))}
+    </Container>
+  )
+}
+
+export default Recommendation
